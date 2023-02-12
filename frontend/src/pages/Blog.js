315bcb18b63e5724d4f2730407a0fb5bbb48239a
Rawand A.Ali <rawand.abubakir@gmail.com>
2023-02-12 21:34:51 +0300

    modified:   frontend/src/pages/Blog.js

--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -10,13 +10,12 @@ export default function Blog() {
 
   useEffect(() => {
     dispatch(getposts());
-    console.log('useeffect run')
   },[dispatch]);  
 
   return (
-    <div className=" bg-secondary min-h-screen min-w-screen">
-     <div className=' m-4  grid md:grid-cols-3 xl:grid-cols-4 sm:grid-cols-2 sm:gap-4 '>
-     {posts &&
+   <div className=" bg-secondary min-h-screen min-w-screen">
+    <div className=' m-4  grid md:grid-cols-3 xl:grid-cols-4 sm:grid-cols-2 sm:gap-4 '>
+    {posts &&
           posts.map((post) => (
             <BlogCard post={post} key={post._id} />
           ))}
